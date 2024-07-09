2e0c20b41bf3ebbe0bb912f2fd74b3ac8682618d
Serban Untu <133024956+SerbanUntu@users.noreply.github.com>
2024-07-09 15:46:48 +0300

    feat: reallocation and testing

--- hashmap.mjs
+++ hashmap.mjs
@@ -1,11 +1,12 @@
 export class HashMap {
 	#buckets = []
 	#capacity = 16
+	#loadFactor = 0.75
 
 	#hash(key) {
 		let hashCode = 0
 
-		const primeNumber = 47
+		const primeNumber = 31
 		for (let i = 0; i < key.length; i++) {
 			hashCode = primeNumber * hashCode + key.charCodeAt(i)
 			hashCode %= this.#capacity
@@ -24,11 +25,18 @@ export class HashMap {
 		if (this.#buckets[hashCode] && this.#buckets[hashCode][0] === key) {
 			// Update
 			this.#buckets[hashCode][1] = value
+			return
 		} else if (this.#buckets[hashCode]) {
 			//! Collision
+			this.#reallocate()
+			this.set(key, value)
 		} else {
 			// New bucket
 			this.#buckets[hashCode] = [key, value]
+		}
+
+		if (this.length() > this.#loadFactor * this.#capacity) {
+			this.#reallocate()
 		}
 	}
 
@@ -104,5 +112,14 @@ export class HashMap {
 			if (this.#buckets[i]) result.push([this.#buckets[i][0], this.#buckets[i][1]])
 		}
 		return result
+	}
+
+	#reallocate() {
+		let entries = this.entries()
+		this.clear()
+		this.#capacity *= 2
+		entries.forEach(entry => {
+			this.set(...entry)
+		})
 	}
 }
--- hashmap.test.mjs
+++ hashmap.test.mjs
@@ -1,18 +1,36 @@
 import { HashMap } from './hashmap.mjs'
 
-let map = new HashMap()
-map.set('something', 123)
-map.set('something', 234)
-map.set('one', 5)
-map.set('five', 5)
+const test = new HashMap()
+test.set('apple', 'red')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown')
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+test.set('grape', 'purple')
+test.set('hat', 'black')
+test.set('ice cream', 'white')
+test.set('jacket', 'blue')
+test.set('kite', 'pink')
+test.set('lion', 'golden')
+test.set('dog', 'blue')
+test.set('carrot', 'green')
+test.set('kite', 'red')
+test.set('frog', 'yellow')
+test.set('moon', 'silver')
+test.set('hat', 'grey')
+test.set('jacket', 'black')
+test.set('banana', 'green')
 
-console.log(map.get('two')) // null
-console.log(map.get('something')) // 234
-console.log(map.remove('four')) // false
-console.log(map.remove('five')) // true
-console.log(map.has('one')) // true
-console.log(map.has('five')) // false
-console.log(map.length()) // 2
-console.log(map.keys()) // [ 'one', 'something' ]
-console.log(map.values()) // [ 5, 234 ]
-console.log(map.entries()) // [ [ 'one', 5 ], [ 'something', 234 ] ]
+console.log(test.get('kite')) // red
+console.log(test.has('banana')) // true
+console.log(test.has('bird')) // false
+console.log(test.remove('moon')) // true
+console.log(test.remove('sky')) // false
+console.log(test.length()) // 12
+console.log(test.keys()) // [ 'carrot', 'grape', 'jacket', 'kite', 'elephant', 'apple', 'hat', 'lion', 'frog', 'banana', 'ice cream', 'dog' ]
+console.log(test.values()) // [ 'green', 'purple', 'black', 'red', 'gray', 'red', 'grey', 'golden', 'yellow', 'green', 'white', 'blue' ]
+console.log(test.entries()) // [ [ 'carrot', 'green' ], [ 'grape', 'purple' ], [ 'jacket', 'black' ], [ 'kite', 'red' ], [ 'elephant', 'gray' ], [ 'apple', 'red' ], [ 'hat', 'grey' ], [ 'lion', 'golden' ], [ 'frog', 'yellow' ], [ 'banana', 'green' ], [ 'ice cream', 'white' ], [ 'dog', 'blue' ] ]
+
+test.clear()
+console.log(test.length()) // 0
